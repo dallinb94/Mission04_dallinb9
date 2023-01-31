d0b5ed56e0d4fb795b67535e2e2ddbc2e7fa9ffa
Dallin Blanc <dallinb94@gmail.com>
2023-01-30 22:30:31 -0700

    blah

--- Mission04_dallinb9/wwwroot/js/GradeCalc.js
+++ Mission04_dallinb9/wwwroot/js/GradeCalc.js
@@ -1,5 +1,54 @@
 ﻿$(document).ready(function () {
-    $("#calculate").click(function () {
-        var examWeight
+    $("#calcButton").click(function () {
+
+        var assignments = parseFloat($("#assignment").val()) * 0.5;
+        var groupProject = parseFloat($("#groupProject").val()) * 0.1;
+        var quizzes = parseFloat($("#quiz").val()) * 0.1;
+        var midtermExam = parseFloat($("#midtermExam").val()) * 0.1;
+        var finalExam = parseFloat($("#finalExam").val()) * 0.1;
+        var intex = parseFloat($("#intex").val()) * 0.1;
+
+        var total = assignments + groupProject + quizzes + midtermExam + finalExam + intex;
+
+        var letterGrade = "";
+
+        if (total >= 94) {
+            letterGrade = "A";
+        }
+        else if (total >= 90) {
+            letterGrade = "A-";
+        }
+        else if (total >= 87) {
+            letterGrade = "B+";
+        }
+        else if (total >= 84) {
+            letterGrade = "B";
+        }
+        else if (total >= 80) {
+            letterGrade = "B-";
+        }
+        else if (total >= 77) {
+            letterGrade = "C+";
+        }
+        else if (total >= 74) {
+            letterGrade = "C";
+        }
+        else if (total >= 70) {
+            letterGrade = "C-";
+        }
+        else if (total >= 67) {
+            letterGrade = "D+";
+        }
+        else if (total >= 64) {
+            letterGrade = "D";
+        }
+        else if (total >= 60) {
+            letterGrade = "D-";
+        }
+        else {
+            letterGrade = "E";
+        }
+
+        $("#finalGrade").html("Final Grade: " + letterGrade + " " + total.toFixed(2) + "%");
     });
 });
